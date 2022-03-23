5fde0acb46bbf40a489d235e5c2286b58c9c301e
Austin <ofthevalley111@gmail.com>
2022-03-23 16:59:45 -0500

    Finished making a welcome display, a display of the rules, anda method that shows player amount. We also started working on a method to choose who goes first.

--- Game.py
+++ Game.py
@@ -1,3 +1,8 @@
+import random
+
+from Player import Player 
+
+
 class Game:
     def __init__(self):
         pass
@@ -10,10 +15,23 @@ class Game:
         print ("1.The Game is played to best of three!-The first player to reach two wins, wins the Game."
                 "2.Heres how to win-"
                    "-Rock crushes Scissors and Lizard"
-                   "-Paper covers rock and disproves Spock.
-                   "-Scissors cuts paper and decapitates Lizard.
-                   "-Lizard eats Paper and poisons Spock.
+                   "-Paper covers rock and disproves Spock."
+                   "-Scissors cuts paper and decapitates Lizard."
+                   "-Lizard eats Paper and poisons Spock."
                    "-Spock vaporizes Rock and smashes Scissors")
 
-                       
-                 
+
+    def player_amount(self):
+        user_input = input(f'Is there one or two players that are playing this game? Press  1  or  2    ')
+        if user_input == 1:
+            pass
+        elif user_input == 2:
+            pass
+        else:
+            pass
+
+
+    def who_goes_first(self):
+        random.choice[Players] # Players is a list, create a list of players
+        print(f'Player {self.random.choice} has been chose to go first.')
+        pass
---
+++ Player.py
@@ -0,0 +1,15 @@
+
+
+class Player:
+    def __init__(self, name):
+        self.name = ''
+        self.choice = []
+        self.score = ''
+        pass
+
+
+    def player_turn(self):
+        self.gesture = None
+        pass
+    
+
